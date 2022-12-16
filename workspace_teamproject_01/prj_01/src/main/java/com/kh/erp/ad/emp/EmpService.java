9bfaa8873a34aed8c6ab6e16aad6598b268f61ad
jiwoo <115950651+erase-yoon@users.noreply.github.com>
2022-12-16 16:50:44 +0900

    2022.12.16_16_50

--- workspace_teamproject_01/prj_01/src/main/java/com/kh/erp/ad/emp/EmpService.java
+++
@@ -1,8 +0,0 @@
-package com.kh.erp.ad.emp;
-
-public interface EmpService {
-	RegDTO getEmpListCorr(String no_emp);
-	int updateList (RegDTO regDTO);
-	
-	int deleteList (RegDTO regDTO);
-}
--- workspace_teamproject_01/prj_01/src/main/java/com/kh/erp/emp/sal/EmpSalService.java
+++
@@ -1,5 +0,0 @@
-package com.kh.erp.emp.sal;
-
-public interface EmpSalService {
-	//EmpSalDTO getEmpSal(String no_emp, boolean isEmpSalary);
-}
